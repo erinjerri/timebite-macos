dd76a4094ccdfcd54a82af73c9791dc2f64c131e
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2026-08-13 09:43:50 -0700

    Initial Timebite macOS commit

---
+++ timebite-macosTests/timebite_macosTests.swift
@@ -0,0 +1,19 @@
+//
+//  timebite_macosTests.swift
+//  timebite-macosTests
+//
+//  Created by Erin Jerri on 8/13/26.
+//
+
+import Testing
+@testable import timebite_macos
+
+struct timebite_macosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
